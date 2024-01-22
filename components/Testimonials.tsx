b2aae1b3517f2bc71bf342120abb545080486e44
Seif Eldin Sameh <seiffsameh00@gmail.com>
2024-01-22 21:49:58 +0200

    Seif Eldin Sameh

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -189,6 +189,74 @@ export default function Testimonials() {
             <p className="text-sm uppercase">CS Student Shrouk Academy</p>
           </div>
         </div>
+        {/* box 6 */}
+        <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+          <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-900">
+            <p className="relative px-6 py-1 text-lg italic text-center text-gray-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                className="w-8 h-8 text-[#00b5e7]"
+              >
+                <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+              </svg>
+              Seif sameh is a person who is meticulous in his process and
+              deadlines, and does his work in the fastest time ، Moreover, He is
+              an excellent partner, a loyal work friend, and he is the heart of
+              his Brother🤍
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                className="absolute right-0 w-8 h-8 text-[#00b5e7]"
+              >
+                <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+              </svg>
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-[#00b5e7] dark:text-gray-900">
+            <p className="text-xl font-semibold leadi">Ahmed Osama</p>
+            <p className="text-sm uppercase">Lead in OI ROV</p>
+          </div>
+        </div>
+        {/* box 7 */}
+        <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+          <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 bg-gray-900">
+            <p className="relative px-6 py-1 text-lg italic text-center text-gray-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                className="w-8 h-8 text-[#00b5e7]"
+              >
+                <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+              </svg>
+              Seif Eldin Sameh is an exceptional Full Stack Web Developer who
+              consistently exceeds expectations. His technical acumen, combined
+              with a keen eye for detail, results in web solutions that are not
+              only functional but also aesthetically pleasing. Seif&apoos;s
+              dedication to staying abreast of industry trends ensures that his
+              work is always at the forefront of innovation.
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                className="absolute right-0 w-8 h-8 text-[#00b5e7]"
+              >
+                <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+              </svg>
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center p-8 rounded-b-lg bg-[#00b5e7] dark:text-gray-900">
+            <p className="text-xl font-semibold leadi">Soo Eldek</p>
+            <p className="text-sm uppercase">CS Student Obour Institutes</p>
+          </div>
+        </div>
       </div>
     </section>
   );
